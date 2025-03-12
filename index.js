e0f2abe4b2c2a306ee4fc850481b16fbdd9a2ba8
Gabriel Rohden <gabriel_rohden@outlook.com>
2025-03-12 12:47:04 -0300

    chore: enable ui animator

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-import { AppRegistry } from 'react-native';
+import { AppRegistry, Platform, UIManager } from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/app';
+
+if (Platform.OS === 'android') {
+  UIManager.setLayoutAnimationEnabledExperimental?.(true);
+}
 
 AppRegistry.registerComponent(appName, () => App);
